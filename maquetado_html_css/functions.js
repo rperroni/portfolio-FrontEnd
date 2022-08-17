678ad1c3678a748ca8fe0c92f1e87e070cab37b0
rperroni <rperroni@fi.uba.ar>
2022-08-17 12:49:06 -0300

    agrego maquetado en html y css

---
+++ maquetado_html_css/functions.js
@@ -0,0 +1,4 @@
+function darkModeActivation(){
+    document.body.classList.toggle("body-dark-mode");
+    /*document.getElementsByClassName("information-block").classList.toggle("information-block-dark");*/
+};
